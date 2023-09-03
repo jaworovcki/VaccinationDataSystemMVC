a90226cf32a31acc28d3f33a702099d9e2ef2bfe
Andrii <jaworovcki@gmail.com>
2023-09-03 10:44:37 +0300

    Add project files.

---
+++ VaccinationDataSystem/DataAccess/VaccinationDataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VaccinationDataSystem.DataAccess
+{
+	public class VaccinationDataContext : DbContext
+	{
+        public VaccinationDataContext(DbContextOptions<VaccinationDataContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
