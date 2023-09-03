90f90f951af972a95c9e010a1d46000efe4e0436
Andrii <jaworovcki@gmail.com>
2023-09-03 18:12:05 +0300

    Inititalize VaccineController and VaccineRepository

---
+++ VaccinationDataSystem/Controllers/VaccineController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VaccinationDataSystem.DataAccess;
+
+namespace VaccinationDataSystem.Controllers
+{
+	public class VaccineController : Controller
+	{
+		private readonly VaccinationDataContext context;
+
+		public VaccineController(VaccinationDataContext context)
+        {
+			this.context = context;
+		}
+
+
+    }
+}
---
+++ VaccinationDataSystem/Sevices/VaccineRepository.cs
@@ -0,0 +1,16 @@
+﻿using VaccinationDataSystem.DataAccess;
+
+namespace VaccinationDataSystem.Sevices
+{
+	public class VaccineRepository
+	{
+		private readonly VaccinationDataContext context;
+
+		public VaccineRepository(VaccinationDataContext context)
+        {
+			this.context = context;
+		}
+
+		
+    }
+}
