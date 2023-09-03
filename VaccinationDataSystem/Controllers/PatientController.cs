5a7181c8e165be0302baea990477b9e474854b48
Andrii <jaworovcki@gmail.com>
2023-09-03 12:36:16 +0300

    Create welcome page

---
+++ VaccinationDataSystem/Controllers/PatientController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VaccinationDataSystem.DataAccess;
+
+namespace VaccinationDataSystem.Controllers
+{
+	public class PatientController : Controller
+	{
+		private readonly VaccinationDataContext context;
+
+		public PatientController(VaccinationDataContext context)
+        {
+			this.context = context;
+		}
+
+    }
+}
